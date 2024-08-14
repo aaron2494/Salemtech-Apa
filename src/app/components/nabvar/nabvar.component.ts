99bac73c09fc6c90f107dc6c9d9b2231a433cdf1
aaron Francolino <aaron.e.francolino@gmail.com>
2024-08-14 14:40:53 -0300

    navbar limpio , ya con logo

--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -1,7 +1,7 @@
 
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { ScrollServiceService } from '../../scroll-service.service';
+
 
 @Component({
   selector: 'app-nabvar',
@@ -14,37 +14,18 @@ export class NabvarComponent implements OnInit {
 
   constructor( private router:Router) { }
   ngOnInit(): void {
-    this.checkScroll();
+   
   }
+
   
-  onNavigate(sectionId: string) {
-    // Navega a la sección especificada
+  onNavigate(sectionId: string): void {
     this.router.navigate([], { fragment: sectionId }).then(() => {
-      this.scrollToElement(sectionId);
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
     });
   }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event): void {
-    this.checkScroll();
-  }
-
-  checkScroll(): void {
-    const navbar = document.getElementById('navbar');
-    if (navbar) {
-      if (window.scrollY > 50) { // Ajusta el valor para cuando el navbar se reduzca
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    }
-  }
-  scrollToElement(elementId: string): void {
-    const element = document.getElementById(elementId);
-    if (element) {
-      // Desplazamiento suave al elemento
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
 }
+ 
 
