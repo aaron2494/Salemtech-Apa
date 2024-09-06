fbcffede8b1917662bfe69ff0af5c6c89953ffa5
aaron Francolino <aaron.e.francolino@gmail.com>
2024-09-06 18:59:54 -0300

    email-funtion ready

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import emailjs from 'emailjs-com';
+import {  FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  constructor(private http: HttpClient) { }
+
     consulta = {
       nombre: '',
       email: '',
@@ -19,52 +21,34 @@ export class ContactComponent {
       asunto: '',
       mensaje: ''
     };
-  
-    enviarConsulta(contactForm: NgForm) {
-      if (contactForm.valid) {
-        const templateParams = {
-          from_name: this.consulta.nombre,
-          from_email: this.consulta.email,
-          phone: this.consulta.telefono,
-          subject: this.consulta.asunto,
-          message: this.consulta.mensaje
-        };
-  
-        emailjs.send('service_23f1zaj', 'template_wb0ybs9', templateParams, 'u8Vz8nts2MqNMtH5E')
-          .then(response => {
-            console.log('Correo enviado con éxito', response);
-            Swal.fire({
-              icon: 'success',
-              title: '¡Correo Enviado!',
-              text: 'Tu consulta ha sido enviada con éxito.',
-              confirmButtonText: 'Cerrar'
-            }).then(() => {
-              this.limpiarFormulario();
-            });
-          })
-          .catch(error => {
-            console.error('Error al enviar el correo', error);
-            Swal.fire({
-              icon: 'error',
-              title: 'Error',
-              text: 'Hubo un problema al enviar el correo. Por favor, intenta nuevamente.',
-              confirmButtonText: 'Cerrar'
-            });
-          });
-      } else {
-        console.error('Por favor, completa todos los campos requeridos.');
-        // Marca todos los campos como tocados para que se muestre la validación
-        Object.values(contactForm.controls).forEach(control => control.markAsTouched());
+
+    enviarConsulta(form: any) {
+      if (form.valid) {
+        this.http.post<{ message: string }>('https://backend-email.vercel.app/send-email', this.consulta)
+          .subscribe(
+            response => {
+              // Mostrar SweetAlert de éxito
+              Swal.fire({
+                icon: 'success',
+                title: 'Correo enviado con éxito',
+                text: response.message,
+                confirmButtonText: 'OK'
+              });
+    
+              // Reiniciar el formulario
+              form.reset();
+            },
+            error => {
+              // Mostrar SweetAlert de error
+              Swal.fire({
+                icon: 'error',
+                title: 'Error al enviar el correo',
+                text: 'Ocurrió un problema al intentar enviar el correo. Por favor, intenta nuevamente.',
+                confirmButtonText: 'OK'
+              });
+              console.log(error);
+            }
+          );
       }
-    }
-  
-    limpiarFormulario() {
-      this.consulta = {
-        nombre: '',
-        email: '',
-        telefono: '',
-        asunto: '',
-        mensaje: ''
-      };
-    }
   }
+ }
