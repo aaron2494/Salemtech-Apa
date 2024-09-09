a04857f6157050f4eff950156bebf8b8737a7cb5
aaron Francolino <aaron.e.francolino@gmail.com>
2024-09-09 10:57:10 -0300

    update angular and CEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  RouterOutlet } from '@angular/router';
+import {  NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { MainComponent } from './components/main/main.component';
 import { Seccion2Component } from './components/seccion2/seccion2.component';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { NabvarComponent } from "./components/nabvar/nabvar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 
 
@@ -31,9 +32,18 @@ import { FooterComponent } from "./components/footer/footer.component";
   ]
 })
 
-export class AppComponent {
-  constructor(private cdr:ChangeDetectorRef){}
-  title = 'Salemtech';
+export class AppComponent implements OnInit ,AfterViewInit {
+  constructor(private cdr:ChangeDetectorRef, private meta: Meta,private title: Title, private router: Router ){}
+
+  ngOnInit(): void {
+     this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateMetaTagsBasedOnRoute(event.urlAfterRedirects);
+      }
+    });
+
+  }
+ 
   getRouteAnimation() {
     // Devuelve el nombre del trigger de animación que quieres usar
     return "fadeAnimation"; // Usar la propiedad del trigger directamente
@@ -41,5 +51,27 @@ export class AppComponent {
   ngAfterViewInit() {
     // Forzar la detección de cambios después de que la vista se ha inicializado
     this.cdr.detectChanges();
+  }
+
+  private updateMetaTagsBasedOnRoute(url: string): void {
+    switch (url) {
+      case '/home':
+        this.title.setTitle('Página de Inicio');
+        this.meta.updateTag({ name: 'description', content: 'calidad , experiencia en procesos, soporte de elite' });
+        break;
+      case '/presentation':
+        this.title.setTitle('Sobre Nosotros');
+        this.meta.updateTag({ name: 'description', content: 'Realizamos implementaciones tecnológicas en control de procesos, excelencia operacional, optimización de plantas de procesos de la industria mediana, pesada y desarrollo de sistemas informáticos.' });
+        break;
+      case '/seccion2':
+        this.title.setTitle('nuestro equipo');
+        this.meta.updateTag({ name: 'description', content: 'Nuestro equipo está formado por personal multidisciplinario experimentado y dotados de una fuerte orientación al Cliente sobre una cultura de trabajo bajo normas internacionales de Calidad, Seguridad, Salud y Ambiente.' });
+        break;
+      // Agrega más casos según sea necesario
+      default:
+        this.title.setTitle('Salemtech');
+        this.meta.updateTag({ name: 'description', content: 'presatdores de servicios tecnologicos a la medida del cliente' });
+        break;
+    }
   }
 }
