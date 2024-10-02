76651212ff0187c7fbb669a189462c124702bc65
aaron2494 <aaron.e.francolino@gmail.com>
2024-10-02 19:20:18 -0300

    actualizacion en desktop

--- src/app/components/nabvar/procesos/procesos.component.ts
+++ src/app/components/nabvar/procesos/procesos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-procesos',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './procesos.component.html',
   styleUrl: './procesos.component.scss'
 })
-export class ProcesosComponent {
+export class ProcesosComponent implements AfterViewInit  {
+
+
+  ngAfterViewInit(): void {
+     const carouselElement = document.querySelector('#carouselExampleIndicators');
+    const itemsPerSlide = window.innerWidth >= 768 ? 2 : 1;
+    const carouselItems = carouselElement?.querySelectorAll('.carousel-item');
+
+    carouselElement?.addEventListener('slide.bs.carousel', (event: any) => {
+      const totalItems = carouselItems?.length;
+      const currentIndex = event.to;
+
+      if (currentIndex >= totalItems! - (itemsPerSlide - 1)) {
+        event.preventDefault(); // Detiene el comportamiento por defecto del carrusel
+        const carouselInner = carouselElement.querySelector('.carousel-inner');
+        carouselInner?.insertAdjacentHTML('beforeend', carouselInner.children[0].outerHTML); // Clona el primer elemento
+        carouselInner?.removeChild(carouselInner.children[0]); // Remueve el primer elemento
+      }
+    });
+  }
 
 }
